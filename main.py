5826846eee13348735877901f5efc2ea57b4dca5
ricardopitanga <ricardopitanga@yahoo.com.br>
2020-08-20 17:24:17 -0300

    master

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print ("teste")
+import requests
+import time
+
+#Ler as mensagens que estão chegando no boot
+
+while true:
+
+token = ´1013804190:AAGZK5CCIwFackMpWbhEF3cML3YiSGyy9M´ 
+url_base = f´https://api.telegram.org/bot{token}/getUpdates´
+resultado = requests.get(url_base) 
+print(resulta.json())
+time.sleep(10)  
