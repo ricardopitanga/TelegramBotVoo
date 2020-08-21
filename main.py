1200cf400d2d1c17fa0eac80aa510dd2079c734c
ricardopitanga <ricardopitanga@yahoo.com.br>
2020-08-20 23:34:12 -0300

    commit 1

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ import time
 
 #Ler as mensagens que estão chegando no boot
 
-while true:
-
-token = ´1013804190:AAGZK5CCIwFackMpWbhEF3cML3YiSGyy9M´ 
-url_base = f´https://api.telegram.org/bot{token}/getUpdates´
-resultado = requests.get(url_base) 
-print(resulta.json())
-time.sleep(10)  
+while True:
+    token = "1013804190:AAGZK5CCIwFackMpWbhEF3cML3YiSGyy9MY"
+    url_base = f'https://api.telegram.org/bot{token}/getUpdates'
+    resultado = requests.get(url_base) 
+    print(resultado.json())
+    time. sleep(10) 
